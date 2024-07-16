bf1b26a494f040cd2bcbf778f8a08b599d9f5c27
HONG Yifan <elsk@google.com>
2024-07-15 17:01:27 -0700

    kleaf: init_ddk properly truncates default.xml
    If the generated file is smaller than the
    original file even when a new tag is added
    (for example, useless spaces are trimmed away),
    then the generated file was ill-formed because
    of trailing garbage text. Truncate them.
    
    Test: init_ddk_test
    Change-Id: I7f53c524d212a95efafc0a2c4e2d0607efe2f1a4

--- init/init_ddk_test.py
+++ init/init_ddk_test.py
@@ -317,7 +317,9 @@ local_path_override(
             repo_manifest.parent.mkdir(parents=True, exist_ok=True)
             repo_manifest.write_text(textwrap.dedent("""\
                 <?xml version="1.0" encoding="UTF-8"?>
-                <manifest />
+                <manifest>
+                    <useless                    garbage="true" />
+                </manifest>
             """))
 
             remote_prebuilts_dir = tmp / "remote_prebuilts_dir"
