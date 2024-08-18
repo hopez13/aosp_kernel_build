b3088fec1be8987d1db31af444f3d23d98dbd7a2
Tarun Sahu <tarunsahu@google.com>
2024-08-18 17:02:40 +0000

    [Do Not Merge] Updated the build script to handle the page_size change
    When a config is generated for 4K page and then again re-generated for
    16K page, the value CONFIG_ARM64_VA_BITS_39 doesn't get unset, Which
    after sorting the config, put this value after CONFIG_ARM64_VA_BITS_36
    causing the build system to use 47 bits instead of 36 for 16K page size.
    This change will unblock the 16k page config setup. Right now, this
    seems to be the easiest solution.
    This updates the CONFIG_ARM64_VA_BITS_<VA_BITS> value to "is not set" if
    set to "y", for all the VA_BITS that are not equal to
    CONFIG_ARM64_VA_BITS value.
    
    Bug: 358270050
    Test: make -c build kernel-menuconfig HW=postsil
    This will create the config which will not have any duplicate
    CONFIG_ARM64_VA_BITS_<VA_BITS> entries.
    Change-Id: I26b8d9bcefd80510edc517a064a210f32c042c62
    Signed-off-by: Tarun Sahu <tarunsahu@google.com>

--- build_utils.sh
+++ build_utils.sh
@@ -946,6 +946,22 @@ function menuconfig() {
 
   ${KERNEL_DIR}/scripts/diffconfig -m ${orig_config} ${new_config} > ${changed_config}
   KCONFIG_CONFIG=${new_fragment} ${ROOT_DIR}/${KERNEL_DIR}/scripts/kconfig/merge_config.sh -m ${FRAGMENT_CONFIG} ${changed_config}
+  VA_BITS_VALUE=$(grep '^CONFIG_ARM64_VA_BITS=' ${changed_config} | cut -d'=' -f2)
+  echo "================VA_BITS_VALUE: $VA_BITS_VALUE"
+
+  VA_BITS_LIST="39 36 47"
+  for VA_BITS in $VA_BITS_LIST; do
+	  echo "NOT WORKING $VA_BITS================="
+	  if [ "$VA_BITS" != "$VA_BITS_VALUE"  ]; then
+	    # Create the string to search for
+		STRING="CONFIG_ARM64_VA_BITS_$VA_BITS"
+		# Search for the string in the TMP_FILE
+		if grep -q "^$STRING=" "${new_fragment}"; then
+			# If the string is found, modify it
+			sed -i "s/^$STRING=.*/# $STRING is not set/" "${new_fragment}"
+		fi
+	  fi
+  done
   sort_config ${new_fragment} > $(realpath ${FRAGMENT_CONFIG})
   set +x
 
